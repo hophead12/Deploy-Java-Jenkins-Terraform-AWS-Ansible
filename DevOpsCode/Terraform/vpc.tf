c283de3853c0f5e15927d35abba29177a4e7618b
hopead12 <danildote.db@gmail.com>
2023-02-06 19:40:20 +0100

    add right route to vpc.tf

--- DevOpsCode/Terraform/vpc.tf
+++ DevOpsCode/Terraform/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+
   tags = merge(var.common_tag, {Name = "GateWay-Petclinic-${var.current_environment}-${var.current_version}"})
 
 }
@@ -20,7 +20,7 @@ resource "aws_route_table" "route" {
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = merge(var.common_tag, {Name = "Route-Petclinic-${var.current_environment}-${var.current_version}"})
- 
+  
 }
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
@@ -28,9 +28,7 @@ resource "aws_subnet" "main" {
   cidr_block = "10.0.0.0/25"
   tags = merge(var.common_tag, {Name = "Subnet-Petclinic-${var.current_environment}-${var.current_version}"})
   
-  depends_on = [
-    aws_internet_gateway.gw
-  ]
+
 }
 
 resource "aws_subnet" "main2" {
@@ -39,9 +37,7 @@ resource "aws_subnet" "main2" {
   cidr_block = "10.0.0.128/25"
   tags = merge(var.common_tag, {Name = "Subnet2-Petclinic-${var.current_environment}-${var.current_version}"})
   
-  depends_on = [
-    aws_internet_gateway.gw
-  ]
+
 }
 
 resource "aws_network_interface" "main" {
