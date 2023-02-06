7b7e5842820d0ab9b487f4c337f827e06be33562
hopead12 <danildote.db@gmail.com>
2023-02-06 16:53:47 +0100

    add right route to vpc.tf

--- DevOpsCode/Terraform/vpc.tf
+++ DevOpsCode/Terraform/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+
   tags = merge(var.common_tag, {Name = "GateWay-Petclinic-${var.current_environment}-${var.current_version}"})
   depends_on = [
     aws_vpc_main
@@ -22,6 +22,9 @@ resource "aws_route_table" "route" {
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = merge(var.common_tag, {Name = "Route-Petclinic-${var.current_environment}-${var.current_version}"})
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
 }
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
