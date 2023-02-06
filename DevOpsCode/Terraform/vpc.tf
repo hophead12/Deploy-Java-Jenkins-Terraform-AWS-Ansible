660451c55d476cca35c13c80f264784177a8ed2e
hopead12 <danildote.db@gmail.com>
2023-02-06 16:33:42 +0100

    add right route to vpc.tf

--- DevOpsCode/Terraform/vpc.tf
+++ DevOpsCode/Terraform/vpc.tf
@@ -1,5 +1,9 @@
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/24"
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.gw.id
+    }
     instance_tenancy = "default"
     enable_dns_hostnames = true
     tags = merge(var.common_tag, {Name = "VPC-Petclinic-${var.current_environment}-${var.current_version}"})
@@ -11,15 +15,6 @@ resource "aws_internet_gateway" "gw" {
   tags = merge(var.common_tag, {Name = "GateWay-Petclinic-${var.current_environment}-${var.current_version}"})
 }
 
-resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = merge(var.common_tag, {Name = "Route-Petclinic-${var.current_environment}-${var.current_version}"})
-}
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
   availability_zone = data.aws_availability_zones.availability.names[0]
