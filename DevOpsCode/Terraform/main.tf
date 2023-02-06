b29cf1426cd6ac219aa74c9a463c7ca59777fa77
hopead12 <danildote.db@gmail.com>
2023-02-06 15:22:01 +0100

    add vpc.tf and change sshkey, also adddb rds

--- DevOpsCode/Terraform/main.tf
+++ DevOpsCode/Terraform/main.tf
@@ -1,12 +1,13 @@
 provider "aws"{
-
   region     = var.region
 }
 
 
 resource "aws_eip" "web"{
 
-  instance = aws_instance.web.id     # Create elastic IP in aws and attach it on instance web
+  #instance = aws_instance.web.id     # Create elastic IP in aws and attach it on instance web
+  network_interface  = aws_network_interface.main.id
+  associate_with_private_ip = aws_network_interface.main.private_ip
   tags     = merge(var.common_tag, {Name = "Server IP web-${var.current_environment}"})
 }
 
@@ -14,35 +15,46 @@ resource "aws_eip" "web"{
 resource "aws_instance" "web" {
     ami                    = data.aws_ami.latest_ubuntu.id
     instance_type          = var.instance_type
-    vpc_security_group_ids = [aws_security_group.web.id]
     tags                   = merge(var.common_tag, {Name = "Web-${var.current_environment}-${var.current_version}-${var.current_build}"})
     availability_zone      = data.aws_availability_zones.availability.names[0]
     key_name               = aws_key_pair.generated_key_web.key_name
+    
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index         = 0
+  }
     
   lifecycle {
    create_before_destroy = true # the server will be destroyed after create new server
   }
 
   depends_on = [
-    aws_db_instance.db
+    aws_db_instance.db,
+    aws_internet_gateway.gw
   ]
 }
 
 
 resource "aws_db_instance" "db" {
   allocated_storage       = 10
-  db_name                 = "mydb"
+  
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  
+  identifier              = "db-petclinic" 
   engine                  = "mysql"
   engine_version          = "5.7"
-  instance_class          = var.instance_type
-
-  username                = "petclinic"
-  password                = "petclinic"
+  instance_class          = "db.t3.micro"
   parameter_group_name    = "default.mysql5.7"
   skip_final_snapshot     = true
+  #publicly_accessible     = true
+
+  db_name                 = "petclinic"
+  username                = "petclinic"
+  password                = "petclinic"
+
 
   vpc_security_group_ids  = [aws_security_group.db.id]
-  availability_zone       = data.aws_availability_zones.availability.names[1]
+  availability_zone       = data.aws_availability_zones.availability.names[0]
 
 }                    
 
@@ -63,9 +75,9 @@ resource "aws_db_instance" "db" {
 
 
 resource "aws_security_group" "db" {
-  name        = "${var.current_version} security group DB -${var.current_environment}"
-  description = "SecurityGroup for db Dev "
-  
+  name        = "Security group DB-${var.current_environment}-${var.current_version}"
+  description = "SecurityGroup for db"
+  vpc_id      = aws_vpc.main.id
   dynamic "ingress" {
     for_each = var.allow_ports_db
     content {
@@ -90,9 +102,9 @@ resource "aws_security_group" "db" {
 
 
 resource "aws_security_group" "web" {
-  name        = "${var.current_version} security group Web -${var.current_environment}"
-  description = "SecurityGroup for web Dev "
-  
+  name        = "Security group Web-${var.current_environment}-${var.current_version}"
+  description = "SecurityGroup for web"
+  vpc_id      = aws_vpc.main.id
   dynamic "ingress" {
     for_each = var.allow_ports
     content {
