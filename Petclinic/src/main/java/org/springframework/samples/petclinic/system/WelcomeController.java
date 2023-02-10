a1128dfe959819826e44c039af5830e733866817
hopead12 <danildote.db@gmail.com>
2023-02-10 13:42:20 +0100

    test print hello

--- Petclinic/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ Petclinic/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -25,7 +25,7 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome(Model model) {
-        model.addAttribute("version", System.getenv("HOME"));
+        model.addAttribute("version", System.getenv("VERSION"));
         return "welcome";
     }
 }
