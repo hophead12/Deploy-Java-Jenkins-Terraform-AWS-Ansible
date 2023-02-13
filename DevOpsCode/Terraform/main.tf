9d1a0bb15bff441164a7b80849a6ccad6f55dce7
hopead12 <danildote.db@gmail.com>
2023-02-13 09:56:01 +0100

    add elb asg launch configuration

--- DevOpsCode/Terraform/main.tf
+++ DevOpsCode/Terraform/main.tf
@@ -61,12 +61,6 @@ resource "aws_elb" "web" {
       lb_port           = 80
       lb_protocol       = "http"
     }
-    listener {
-      instance_port     = 80
-      instance_protocol = "https"
-      lb_port           = 443
-      lb_protocol       = "https"
-    }
     health_check {
       healthy_threshold   = 2
       unhealthy_threshold = 2
