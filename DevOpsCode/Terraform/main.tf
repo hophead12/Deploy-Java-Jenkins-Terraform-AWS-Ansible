1c3b345854606fdab0ba4821b4e3ddfe033af62c
hopead12 <danildote.db@gmail.com>
2023-02-13 09:49:03 +0100

    add elb asg launch configuration

--- DevOpsCode/Terraform/main.tf
+++ DevOpsCode/Terraform/main.tf
@@ -56,10 +56,16 @@ resource "aws_elb" "web" {
     security_groups = [aws_security_group.web.id]
     subnets = [aws_subnet.main.id, aws_subnet.main2.id, aws_subnet.main3.id]
     listener {
-      instance_port     = 22
-      instance_protocol = "tcp"
-      lb_port           = 22
-      lb_protocol       = "tcp"
+      instance_port     = 80
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+    }
+    listener {
+      instance_port     = 80
+      instance_protocol = "https"
+      lb_port           = 443
+      lb_protocol       = "https"
     }
     health_check {
       healthy_threshold   = 2
